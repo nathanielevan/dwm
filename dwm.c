cfb7b85e23ffc3821b4f46b38ccaa81b6d6c050a
Nathaniel Evan <nathanielevan@zohomail.com>
2021-03-23 22:16:54 +0700

    Fixed uneven left and right status text padding

--- dwm.c
+++ dwm.c
@@ -849,7 +849,7 @@ drawbar(Monitor *m)
 	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext);
-		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
+		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2, stext, 0);
 	}
 
 	resizebarwin(m);
