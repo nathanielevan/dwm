91f0fe74d9d276bb04644ca418b4d965c58714ba
Nathaniel Evan <nathanielevan@zohomail.com>
2021-04-17 16:28:17 +0700

    Super+d now launches j4-dmenu-desktop

--- config.def.h
+++ config.def.h
@@ -70,12 +70,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", dmenuprompt, "-fn", dmenufont, "-nb", nord[0], "-nf", nord[4], "-sb", nord[9], "-sf", nord[0], NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -p \"run:\"", "--term", "st", NULL };
 
 #include <X11/XF86keysym.h>
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = j4dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
