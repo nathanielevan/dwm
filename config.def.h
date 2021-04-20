6fbe2a4833a26f35979b16000725f3f1b4c7dc01
Nathaniel Evan <nathanielevan@zohomail.com>
2021-04-20 18:22:06 +0700

    Adds rule for firefox about window

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	{ "firefox",  NULL,       "About Mozilla Firefox",  0,    1,     1,           -1 },
 };
 
 /* layout(s) */
