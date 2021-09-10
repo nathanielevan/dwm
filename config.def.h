d6ac0cc7c8cb2ed9eb2a5acb6166d236657e6d64
Nathaniel Evan <nathanielevan@zohomail.com>
2021-09-10 16:39:22 +0700

    Applies patch dwm-swallow-20201211-61bb8b2.diff

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const Gap default_gap        = {.isgap = 1, .realgap = 12, .gappx = 12};
 static const unsigned int snap      = 0;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 6;   /* systray spacing */
@@ -38,9 +39,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "firefox",  NULL,       "About Mozilla Firefox",  0,    1,     1,           -1 },
+	/* class     instance  title           tags mask  iscentered  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         0,          1,          0,           0,        -1 },
+	{ "firefox", NULL,     NULL,           0,         0,          0,          0,          -1,        -1 },
+	{ "st-256color", NULL, NULL,           0,         0,          0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
