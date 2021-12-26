342506b47b890d3083001422dc42c302752f7deb
Nathaniel Evan <nathanielevan@zohomail.com>
2021-12-26 16:00:05 +0700

    Changes colour scheme

--- config.def.h
+++ config.def.h
@@ -20,15 +20,11 @@ static const int vertpadbar         = 6;        /* vertical padding for statusba
 static const char *fonts[]          = { "Iosevka Nerd Font:size=11" };
 static const char dmenufont[]       = "Iosevka Nerd Font:size=11";
 static const char dmenuprompt[]     = "run:";
-static const char* const nord[]     = { "#2e3440", "#3b4252", "#434c5e", "#4c566a",
-                                        "#d8dee9", "#e5e9f0", "#eceff4",
-                                        "#8fbcbb", "#88c0d0", "#81a1c1", "#5e81ac",
-                                        "#bf616a", "#d08770", "#ebcb8b", "#a3be8c", "#b48ead" };
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { nord[4], nord[0],  nord[0]   },
-	[SchemeSel]  = { nord[0], nord[9],  nord[9]   },
-	[SchemeUrg]  = { nord[0], nord[11], nord[11]  },
+	[SchemeNorm] = { "#fff6e6", "#0b1726", "#0b1726" },
+	[SchemeSel]  = { "#0b1726", "#e9986d", "#e9986d" },
+	[SchemeUrg]  = { "#fff6e6", "#c20f47", "#c20f47" },
 };
 
 /* tagging */
