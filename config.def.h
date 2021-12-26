a5d445e12d31e4c595992ef0b11f0ecd1b44935c
Nathaniel Evan <nathanielevan@zohomail.com>
2021-12-26 16:52:34 +0700

    Simplifies dmenu command

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", dmenuprompt, "-fn", dmenufont, "-nb", nord[0], "-nf", nord[4], "-sb", nord[9], "-sf", nord[0], NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", dmenuprompt, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -p \"drun:\"", "--term", "st", "--no-generic", NULL };
 
