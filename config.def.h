a4a2ca90162edf3a8dfc9105c435e167a28b906f
Nathaniel Evan <nathanielevan@zohomail.com>
2022-05-07 10:29:03 +0700

    Use rofi for launching programs

--- config.def.h
+++ config.def.h
@@ -71,14 +71,18 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", dmenuprompt, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -r -p \"drun:\"", "--term", "st", "--no-generic", NULL };
+// static const char *j4dmenucmd[] = { "j4-dmenu-desktop", "--dmenu", "dmenu -i -r -p \"drun:\"", "--term", "st", "--no-generic", NULL };
+static const char *rofidruncmd[] = { "rofi", "-show", "drun", "-modi", "drun,run,window,ssh", NULL };
+static const char *rofiruncmd[] = { "rofi", "-show", "run", "-modi", "drun,run,window,ssh", NULL };
+static const char *rofiwincmd[] = { "rofi", "-show", "window", "-modi", "drun,run,window,ssh", NULL };
 
 #include <X11/XF86keysym.h>
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = j4dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = rofidruncmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = rofiruncmd } },
+	{ MODKEY,                       XK_Escape, spawn,          {.v = rofiwincmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
