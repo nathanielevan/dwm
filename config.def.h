69cf6144c920006612dda8d0dcdb02246118d5f5
Nathaniel Evan <nathanielevan@zohomail.com>
2025-07-16 00:31:59 +0700

    Change backlight brightness command to brightnessctl

--- config.def.h
+++ config.def.h
@@ -128,9 +128,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("powermenu") },
-	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("setmonitor-dmenu") },
-	{ 0, XF86XK_MonBrightnessUp,    spawn,                     SHCMD("light -A 5") },
-	{ 0, XF86XK_MonBrightnessDown,  spawn,                     SHCMD("light -U 5") },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          SHCMD("setmonitor") },
+	{ 0, XF86XK_MonBrightnessUp,    spawn,                     SHCMD("brightnessctl --class=backlight set 5%+") },
+	{ 0, XF86XK_MonBrightnessDown,  spawn,                     SHCMD("brightnessctl --class=backlight set 5%-") },
 	{ 0,                            XK_Print,  spawn,          SHCMD("screenshot-full") },
 	{ ShiftMask,                    XK_Print,  spawn,          SHCMD("screenshot-sel") },
 	{ 0, XF86XK_AudioRaiseVolume,   spawn,                     SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%; pkill -RTMIN+10 dwmblocks") },
